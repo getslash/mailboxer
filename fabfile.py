6a90661e1066f65e005a43cb97c2f5d9a86a0832
Rotem Yaari <vmalloc@gmail.com>
2012-07-04 23:05:52 +0300

    Improve testing fixture to keep panes after commands terminate

--- fabfile.py
+++ fabfile.py
@@ -23,14 +23,12 @@ def testing_fixture():
     local("rm -rf {}".format(_TESTING_MONGO_DB_PATH))
     local("mkdir -p {}".format(_TESTING_MONGO_DB_PATH))
 
-    left_commands = [
+    commands = [
         "nginx -c {}".format(_TESTING_NGINX_CONF_FILE),
         "{} {}/run.py -d".format(sys.executable, os.path.dirname(__file__)),
-        ]
-    right_commands = [
         "mongod --auth --dbpath {}".format(_TESTING_MONGO_DB_PATH),
         ]
-    _run_tmux_session(left_commands, right_commands)
+    _run_tmux_session("autoclave-test", commands)
 
 def deploy():
     user = str(run("id -u -n"))
@@ -68,26 +66,14 @@ def _generate_production_nginx_configuration(installation_root):
                                                 daemon=True, config=config)
 
 
-def _run_tmux_session(left_commands, right_commands):
-    session = ''
-    if right_commands:
-        session += 'tmux selectp -t 0; tmux splitw -hd -p 50 \"%s\"; ' % right_commands[-1]
-    for index, command in enumerate(right_commands[:-1]):
-        session += 'tmux selectp -t 1; tmux splitw -d -p %i \"%s\"; ' % (
-            100 / (len(right_commands) - index),
-            command
-    )
-
-    for index, command in enumerate(left_commands[1:]):
-        session += 'tmux selectp -t 0; tmux splitw -d -p %i \"%s\"; ' % (
-            100 / (len(left_commands) - index),
-            command
-    )
-    if left_commands:
-        session += left_commands[0]
-    args = [
-        'tmux',
-        'new-session',
-        session,
-        ]
-    subprocess.call(args)
+def _run_tmux_session(session_name, commands):
+    with settings(warn_only=True):
+        if local("tmux attach -t {}".format(session_name)).succeeded:
+            return
+    local("tmux new -d -s {}".format(session_name))
+    for i in range(len(commands)-1):
+        local("tmux splitw -t {}".format(i))
+    local("tmux selectl tiled")
+    for index, command in enumerate(commands):
+        local('tmux send -t {} "{}" C-m'.format(index, command))
+    local("tmux attach -t {}".format(session_name))
