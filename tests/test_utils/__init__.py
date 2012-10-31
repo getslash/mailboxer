20c8982b8a20c7300d9e62c7b7ced4dc9bb0d4e3
Rotem Yaari <vmalloc@gmail.com>
2012-10-31 23:16:25 +0200

    Backup/restore configuration during tests

--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -1,12 +1,13 @@
 import requests
 import unittest
 from flask_app import app
+from config import configobj
 
 class TestCase(unittest.TestCase):
     def setUp(self):
         super(TestCase, self).setUp()
         self.app = app.app.test_client()
-    def set_config(self, config, key, value):
-        orig_value = getattr(config, key)
-        setattr(config, key, value)
-        self.addCleanup(setattr, config, key, orig_value)
+        configobj.backup()
+    def tearDown(self):
+        configobj.restore()
+        super(TestCase, self).setUp()
