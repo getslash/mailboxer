e6c54b97978dfbf694da7c93b0fdd6229a6ec98a
Rotem Yaari <vmalloc@gmail.com>
2012-11-07 23:21:55 +0200

    Fix celery config

--- deployment/deployment.py
+++ deployment/deployment.py
@@ -74,9 +74,10 @@ def _deploy_celeryd():
     )
     fabtools.require.supervisor.process(
         config.deployment.celeryd.service_name,
-        command="{0}/bin/celeryd --logfile={1} -B --config=flask_app.config.celery".format(config.deployment.virtualenv_path, config.deployment.celeryd.log_path),
-        environment="PYTHONPATH={0}".format(":".join([config.deployment.www_path, config.deployment.src_path])),
+        command="{0}/bin/celeryd -l DEBUG --logfile={1} -B --config=config.celeryconfig".format(config.deployment.virtualenv_path, config.deployment.celeryd.log_path),
+        environment="PYTHONPATH='{0}'".format(":".join([config.deployment.www_path, config.deployment.src_path])),
         user=config.deployment.user,
+        directory="/tmp",
         )
 
 def _deploy_nginx():
