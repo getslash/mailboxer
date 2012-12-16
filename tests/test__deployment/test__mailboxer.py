8b4e61b356231343e3d5ae3e4f390cca164b533a
Rotem Yaari <vmalloc@gmail.com>
2012-12-16 17:52:57 +0200

    Test cleanups

--- tests/test__deployment/test__mailboxer.py
+++ tests/test__deployment/test__mailboxer.py
@@ -5,11 +5,12 @@ import smtplib
 import urlparse
 from unittest import TestCase
 from config import config
+from ..test_utils import config_utils
+from .deployment_test import DeploymentTest
 
-class MailboxrTestBase(TestCase):
+class MailboxrTestBase(DeploymentTest):
     def setUp(self):
         super(MailboxrTestBase, self).setUp()
-        self.url_base = "http://localhost:8080"
         self.email = "{}@somedomain.com".format(uuid1())
         self.fromaddress = "{}@otherdomain.com".format(uuid1())
     def _delete(self, *args, **kwargs):
@@ -19,29 +20,29 @@ class MailboxrTestBase(TestCase):
     def _post(self, *args, **kwargs):
         return self._request("post", *args, **kwargs)
     def _request(self, _method, *args, **kwargs):
-        resp = requests.request(_method, *args, **kwargs)
+        resp = self.request(_method, *args, **kwargs)
         resp.raise_for_status()
         return resp
 
 class MailboxesManagementTest(MailboxrTestBase):
     def test__create_delete_mailbox(self):
         old_mailboxes = self.get_mailboxes()
-        self._post(self.url_base + "/mailboxes/", data=dict(name=self.email))
+        self._post("/mailboxes/", data=dict(name=self.email))
         new_mailboxes = self.get_mailboxes()
         self.assertEquals(len(new_mailboxes), len(old_mailboxes) + 1)
-        self._delete(self.url_base + "/mailboxes/" + self.email)
+        self._delete("/mailboxes/" + self.email)
         self.assertEquals(self.get_mailboxes(), old_mailboxes)
     def test__cannot_create_star_mailbox(self):
         with self.assertRaises(requests.HTTPError) as caught:
-            self._post(self.url_base + "/mailboxes/", data=dict(name="*"))
+            self._post("/mailboxes/", data=dict(name="*"))
         self.assertEquals(caught.exception.response.status_code, httplib.BAD_REQUEST)
     def get_mailboxes(self):
-        return self._get(self.url_base + "/mailboxes/").json
+        return self._get("/mailboxes/").json
 
 class EmailTest(MailboxrTestBase):
     def setUp(self):
         super(EmailTest, self).setUp()
-        self._post(self.url_base + "/mailboxes/", data=dict(name=self.email))
+        self._post("/mailboxes/", data=dict(name=self.email))
     def test__no_emails_by_default(self):
         self.assertEquals([], self.get_all_messages())
     def test__no_unread_emails_by_default(self):
@@ -67,12 +68,12 @@ class EmailTest(MailboxrTestBase):
     def test__recreate_mailbox_deletes_emails(self):
         self._send_email("email")
         self.assertEquals(len(self.get_all_messages()), 1)
-        self._delete(self.url_base + "/mailboxes/" + self.email)
-        self._post(self.url_base + "/mailboxes/", data=dict(name=self.email))
+        self._delete("/mailboxes/" + self.email)
+        self._post("/mailboxes/", data=dict(name=self.email))
         self.assertEquals(len(self.get_all_messages()), 0)
     def get_all_messages(self):
-        return self._get(self.url_base + "/messages/" + self.email).json
+        return self._get("/messages/" + self.email).json
     def get_unread_messages(self):
-        return self._get(self.url_base + "/messages/" + self.email +"/unread").json
+        return self._get("/messages/" + self.email +"/unread").json
     def _send_email(self, msg):
-        smtplib.SMTP("127.0.0.1", 2525).sendmail(self.fromaddress, [self.email], msg)
+        smtplib.SMTP("127.0.0.1", config_utils.get_config_int("smtp_port", 2525)).sendmail(self.fromaddress, [self.email], msg)
--- tests/test__deployment/test__sanity.py
+++
@@ -1,5 +0,0 @@
-from .deployment_test import DeploymentTest
-
-class SanityTest(DeploymentTest):
-    def test__sanity(self):
-        self.request("get", "/").raise_for_status()
